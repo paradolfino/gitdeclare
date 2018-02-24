9ff4409d3f3d187da2a31fc0ccfe98e851463482
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-23 22:34:02 -0600

    fix timestamping

--- gdec.rb
+++ gdec.rb
@@ -52,7 +52,7 @@ class GitDeclare
             @@changes.map! {|item| item = "* #{item.strip}"}
             open('pull_me.txt', 'a') do |file|
                 file.puts "[#{summary}]"
-                file.puts "### [#{current_time}]:"
+                file.puts "### [#{@@time} - #{current_time}]:"
                 file.puts @@changes
                 file.puts
             end
