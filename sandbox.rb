72d91096aedfcf4f36e15a7c830f6a01931c038d
viktharien <viktharien@zoho.com>
2018-04-02 19:54:49 -0500

    testing httparty pushing with sandbox

--- sandbox.rb
+++ sandbox.rb
@@ -125,7 +125,7 @@ class GitDeclare
         else
             open('pull_me.txt', 'w') {|f| f.puts ""}
             @@pushes += 1
-            res = GitDeclare.post(uri, body: {content: "New Declaration"})
+            res = GitDeclare.post(@@uri, body: {content: "New Declaration"})
             body = JSON.parse(res.body)
             @@declare = body["id"]
             
