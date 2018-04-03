f597b3fbc9c09451cea8d34e609d15aab5485b0c
viktharien <viktharien@zoho.com>
2018-04-02 19:19:04 -0500

    begin creation of gitdeclare http

--- command_test.rb
+++ command_test.rb
@@ -1,4 +1,5 @@
 # Use the class methods to get down to business quickly
+require 'httparty'
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
 puts response.body, response.code, response.message, response.headers.inspect
