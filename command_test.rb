899537109bd493ca51ff1fe459519d8f5534f2f2
viktharien <viktharien@zoho.com>
2018-04-02 19:32:42 -0500

    begin creation of gitdeclare http

--- command_test.rb
+++ command_test.rb
@@ -1,3 +1,4 @@
-test = "git rev-parse --abbrev-ref HEAD"
-x = %x(#{test})
-puts x
+@@time = GitDeclare.current_time
+x = %x(git rev-parse --abbrev-ref HEAD)
+@@branch = x
+puts "On #{@@branch} branch"
