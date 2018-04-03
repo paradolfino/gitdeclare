51391a813daba15ec60b955d2cee82792c0152df
viktharien <viktharien@zoho.com>
2018-04-02 19:26:27 -0500

    begin creation of gitdeclare http

--- command_test.rb
+++ command_test.rb
@@ -1,5 +1,8 @@
 # Use the class methods to get down to business quickly
 require 'httparty'
+
+HTTParty.post('http://localhost:3000/declarations.json', body: {content: 'testytesty'})
+
 response = HTTParty.get('http://localhost:3000/declarations.json')
 
 puts response.body
