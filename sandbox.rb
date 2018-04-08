7ee0651025ecb642dd95db03c3dde3e8b255d8b2
viktharien <viktharien@zoho.com>
2018-04-08 17:23:35 -0500

    implement working directory and git uri in post to declarations

--- sandbox.rb
+++ sandbox.rb
@@ -129,7 +129,7 @@ class GitDeclare
         else
             open('pull_me.txt', 'w') {|f| f.puts ""}
             @@pushes += 1
-            res = GitDeclare.post(@@uri, body: {content: "New Declaration"})
+            res = GitDeclare.post(@@uri, body: {content: "New Declaration", directory: "New"})
             body = JSON.parse(res.body)
             @@declare = body["id"]
             
